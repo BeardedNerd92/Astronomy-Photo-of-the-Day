6855fc07c51e40277a0c23a09f3180c9d89f8203
Zack Grooms <IntMain92@gmail.com>
2021-08-16 15:17:38 -0500

    Made changes to the date variable.

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@ const earthData = () =>
     xhr.open('GET', 'https://api.nasa.gov/planetary/apod?api_key=DBtDXqIbf85NTUN18Oejr5oHTOypF9YrLM0iw5dM', true)
     xhr.onload = function() 
     {
+        const date = new Date().toDateString()
         if (this.status == 200) 
         {
             const data = JSON.parse(this.responseText)
             const mainTitle = `Astronomy photo of the day is ${data.title}`
             const img = `<img src=${data.url} alt=${data.title}></img>`
             const explanation = `${data.explanation}`
-            const dateCreated = `Today's date: ${data.date}`
+            const dateCreated = `Today's date: ${date}`
             const author = `&copy; copyrights by ${data.copyright}`
             const subTitle = `What is ${data.title}?`
 
